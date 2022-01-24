069bab6f5388c3541038e9dc30a7dbff48364608
Jacobtread <jacobtread@gmail.com>
2022-01-24 14:34:02 +1300

    Removed authlib circular dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ version = "1.0.0"
 
 dependencies {
     implementation(project(":logger"))
-    implementation(project(":authlib"))
     implementation("com.google.code.gson:gson:2.8.9")
     implementation("com.google.guava:guava:31.0.1-jre")
     implementation("it.unimi.dsi:fastutil:8.5.6")
